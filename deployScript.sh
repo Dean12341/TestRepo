3c85a85b0905c05b89eb6ee48c8b7fd59d2a625b
Dean12341 <dean.warburtonn@gmail.com>
2022-08-10 14:23:21 +0100

    addding deploy script

---
+++ deployScript.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "This is a Deploy Script"
+touch Deploy.exe
+echo "The Current Location is"
+pwd
+echo "The Current User is"
+whoami
+echo "All files in the repo are:"
+ls-files
+
